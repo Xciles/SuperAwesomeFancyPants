da1f0a16e470ad34b28e5ad15d95014d8e3e227e
Xciles <xciles@gmail.com>
2018-04-03 16:13:41 +0200

    SE Track Day 1

---
+++ SuperAwesomeFancyPants/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperAwesomeFancyPants
+{
+    public class Car
+    {
+        //private float _numberOfFuel;
+        public float Fuel { get; private set; }
+        public float Milage { get; private set; }
+
+        public float Range { get { return Fuel * Milage;} }
+
+        //private float _fuel;
+
+        //public float Fuel
+        //{
+        //    get => _fuel;
+        //    set => _fuel = value;
+        //}
+
+        /// <summary>
+        /// THis creates a car using 50 fuel
+        /// </summary>
+        public Car()
+        {
+            Fuel = 50f;
+        }
+
+        /// <summary>
+        /// This creates a car using given fuel
+        /// </summary>
+        /// <param name="fuel">The amount of fuel to craete the car with</param>
+        public Car(float fuel)
+        {
+            Fuel = fuel;
+        }
+
+        public void AddAmmountOfFuel(float ammountOfFuel)
+        {
+            Fuel += ammountOfFuel;
+            //_numberOfFuel = _numberOfFuel + ammountOfFuel;
+        }
+    }
+}
--- SuperAwesomeFancyPants/Program.cs
+++ SuperAwesomeFancyPants/Program.cs
@@ -2,11 +2,64 @@
 
 namespace SuperAwesomeFancyPants
 {
-    class Program
+    // Access Modifiers
+    // private
+    // protected
+    // public 
+    // internal
+    public class Door
     {
-        static void Main(string[] args)
+
+    }
+
+    public class Program
+    {
+        private static int _age;
+        private static string _hans;
+        private static bool _isInEindhoven = false;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ////string name = "Hans";
+            ////bool inEindhoven = false;
+            ////float flo = 1.1f;
+            ////double d = 1.1d;
+            ////decimal dec;
+
+            ////long a = 922337_2036_854_775_807;
+            //////int.MaxValue;
+            ////int number;
+            ////number = 1234;
+            ////number = 43321;
+
+            //////int age = 30;
+            //////age = AddFive(age);
+
+            ////_age = 30;
+
+            //// Write something to Console
+            //Console.WriteLine("Hello Yacht, " + name + ". Welcome");
+            //Console.WriteLine($"Hello Yacht, {name}. {_age}");
+
+            Car car = new Car();
+            
+            car.AddAmmountOfFuel(50.0f);
+            float carFuel = car.Fuel;
+
+            Car anotherCar = new Car(25f);
+
+            Console.ReadKey();
+        }
+
+        public static int AddFive(int value)
+        {
+            _age += 5;
+            value += 5;
+            return value;
         }
     }
 }
