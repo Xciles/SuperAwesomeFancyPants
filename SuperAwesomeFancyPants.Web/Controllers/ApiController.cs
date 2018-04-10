bdc73e61a729786d06d7271da8fd476363a98214
Xciles <xciles@gmail.com>
2018-04-10 14:39:10 +0200

    Staging Api controller

---
+++ SuperAwesomeFancyPants.Web/Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperAwesomeFancyPants.Web.Controllers
+{
+    public class ApiController : Controller
+    {
+        [Route("api/data")]
+        public IActionResult GetSomeData()
+        {
+            return Ok(new SomeObject
+            {
+                Name = "hellao",
+                Age = 383,
+            });
+        }
+    }
+
+    public class SomeObject
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
